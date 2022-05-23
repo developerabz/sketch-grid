c970f8b9e298efd943f665332349e8b61b2e4a8d
Abdullah Yakub <abdullah.yakub@hotmail.com>
2022-05-23 15:03:07 +1000

    add button that generates prompt

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 const sketchpad = document.querySelector(".sketchpad");
 
-let widthSquares = prompt("How many squares wide?", "16");
-let lengthSquares = prompt("How many squares long?", "16")
-for (i=0; i < widthSquares * lengthSquares; i++) {
-    const firstgrid = document.createElement("div");
-    firstgrid.classList.toggle("firstgrid");
-    sketchpad.appendChild(firstgrid);
+let widthSquares;
+let lengthSquares;
+
+
+
+function generate() {
+    
+
+    if (widthSquares >= 100 || lengthSquares >= 100 ||
+        widthSquares < 0 || lengthSquares < 0) {
+        widthSquares = 0;
+        lengthSquares = 0;
+    } else {
+        for (i=0; i < widthSquares * lengthSquares; i++) {
+            const firstgrid = document.createElement("div");
+            firstgrid.classList.toggle("firstgrid");
+            sketchpad.appendChild(firstgrid);
+        }
+    }
 }
+
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    widthSquares = prompt("How many squares wide? (MAX Squares 99)", "16");
+    lengthSquares = prompt("How many squares long? (MAX Squares 99)", "16");
+    generate();
+})
 
 
 const gridboxes = document.querySelectorAll("div.firstgrid");
